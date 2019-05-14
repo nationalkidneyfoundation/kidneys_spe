5b4c0ab86fb0f73dc791861ae509f38b851f8e2e
Jacob Lane <lane.jacob@gmail.com>
2019-05-14 10:02:19 -0400

    debugging

--- templates/kidneys-spe-list-accordion-item.tpl.php
+++ templates/kidneys-spe-list-accordion-item.tpl.php
@@ -2,14 +2,14 @@
 <div id="<?php print $iid;?>" class="padding--md page-break--avoid">
   <a href="#" class="display--flex align-items--baseline color--gray-4" data-toggle="class" data-target="#<?php print $iid;?> i|#<?php print $iid;?> .desc" data-class="hide|hide">
     <div class="cursor--pointer font-size--sm">
-      <i class="icon-right-open color--gray-4"></i>
-      <i class="hide icon-down-open color--orange"></i>
+      <i class="<?php print ($first) ? 'hide' : ''; ?> icon-right-open color--gray-4"></i>
+      <i class="<?php print ($first) ? '' : 'hide'; ?>  icon-down-open color--orange"></i>
     </div>
     <div class="bold padding-left--sm">
       <?php print $title; ?>
     </div>
   </a>
-  <div class="linkHighlight desc hide print--show sm--margin-left--md padding-top--xxs sm--padding-left--sm">
-    <?php print $description; ?>
+  <div class="linkHighlight desc <?php print ($first) ? '' : 'hide'; ?> print--show sm--margin-left--md padding-top--xxs sm--padding-left--sm">
+    <?php print 'testing';//$description; ?>
   </div>
 </div>
