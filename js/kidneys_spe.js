60484d706a2a06026c90e8f198deb01012ce7cd9
Jacob Lane <lane.jacob@gmail.com>
2019-05-31 08:31:29 -0400

    removing console logging in js

--- js/kidneys_spe.js
+++ js/kidneys_spe.js
@@ -2,8 +2,6 @@
   var initialized;
   Drupal.behaviors.kidneys_spe = {
     attach: function (context, settings) {
-
-      console.log('spe cookies');
       // Drop cookie for future use.
       if (settings.kidneys_spe && settings.kidneys_spe.assessments && !$.cookie('spe')) {
         $.cookie('spe', JSON.stringify(settings.kidneys_spe.assessments), { path: '/' });
@@ -12,10 +10,7 @@
       if ($.cookie('spe')) {
         if (!initialized) {
           initialized = true;
-          console.log('has spe cookie');
-
           var spe = JSON.parse($.cookie("spe"));
-
           Object.keys(spe).forEach(function(key,index) {
             if ($( "#spe-return-" + key ).length) {
               var returning = $( "#spe-return-" + key );
